e7fb0be0260b9c72c1b036de4292826ae9b9a481
Justin Leitgeb <justin@stackbuilders.com>
2014-01-28 23:22:26 -0500

    Test cleanup

--- spec/micro_kanren/core_spec.rb
+++ spec/micro_kanren/core_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe MicroKanren::Core do
+
   include MicroKanren::Core
   include MicroKanren::MiniKanrenWrappers
   include MicroKanren::Lisp
@@ -8,42 +9,26 @@ describe MicroKanren::Core do
   include MicroKanren::TestPrograms
   include MicroKanren::TestSupport
 
+  # These tests follow the reference implementation in Scheme located at
+  # https://github.com/jasonhemann/microKanren/blob/master/microKanren-test.scm
+
   it "second-set t1" do
-    res = call_fresh(-> (q) { eq(q, 5) }).call(empty_state)
-
-    # The result should be  following the reference
-    # implementation:
-    # https://github.com/jasonhemann/microKanren/blob/master/microKanren-test.scm#L6
-
-    #       ( ( ( #(0)    . 5 ) ) . 1  )
-    exp = [ [ [ [ uvar(0) , 5 ] ] , 1  ] ]
-    expected = ary_to_sexp(exp)
-
-    lists_equal?(res, expected).must_equal true
+    res = car(call_fresh(-> (q) { eq(q, 5) }).call(empty_state))
+    ast_to_s(res).must_equal '((([0] . 5)) . 1)'
   end
 
   it "second-set t2" do
     res = call_fresh(-> (q) { eq(q, 5) }).call(empty_state)
-
-    # Following reference implementation:
-    # https://github.com/jasonhemann/microKanren/blob/master/microKanren-test.scm#L11
     cdr(res).must_be_nil
   end
 
-  # https://github.com/jasonhemann/microKanren/blob/master/microKanren-test.scm#L13
   it "second-set t3" do
     res = car(a_and_b.call(empty_state))
-
-    #     ( ( ( #(1)    . 5)    ( #(0)   . 7 ) )   . 2)
-    exp = [ [ [ uvar(1) , 5], [ [uvar(0) , 7 ] ] ] , 2]
-    sexp = ary_to_sexp(exp)
-
-    lists_equal?(res, sexp).must_equal true
+    ast_to_s(res).must_equal '((([1] . 5) ([0] . 7)) . 2)'
   end
 
   it "second set t3, take" do
     res = take(1, (a_and_b.call(empty_state)))
-
     ast_to_s(res).must_equal '(((([1] . 5) ([0] . 7)) . 2))'
   end
 
@@ -62,39 +47,21 @@ describe MicroKanren::Core do
   end
 
   it "take 2 a_and_b stream" do
-    # Expected result in scheme:
-    # ((((#(1) . 5) (#(0) . 7)) . 2)
-    #  (((#(1) . 6) (#(0) . 7)) . 2))
+    res = take(2, a_and_b.call(empty_state))
 
     expected_ast_string =
       "(((([1] . 5) ([0] . 7)) . 2) ((([1] . 6) ([0] . 7)) . 2))"
 
-    exp = [[[[ uvar(1), 5 ], [[ uvar(0), 7 ]]], 2],
-           [[[[ uvar(1), 6 ], [[ uvar(0), 7 ]]], 2]]]
-
-    # Sanity check the printed AST representation of our expression above
-    # against what we expect in something closer to scheme.
-    ast_to_s(ary_to_sexp(exp)).must_equal expected_ast_string
-
-    res = take(2, a_and_b.call(empty_state))
-    lists_equal?(res, ary_to_sexp(exp)).must_equal true
+    ast_to_s(res).must_equal expected_ast_string
   end
 
   it "take_all a_and_b stream" do
-    # Expected result in scheme:
-    # ((((#(1) . 5) (#(0) . 7)) . 2)
-    #  (((#(1) . 6) (#(0) . 7)) . 2))
+    res = take_all(a_and_b.call(empty_state))
 
     expected_ast_string =
       "(((([1] . 5) ([0] . 7)) . 2) ((([1] . 6) ([0] . 7)) . 2))"
 
-    exp = [[[[ uvar(1), 5 ], [[ uvar(0), 7 ]]], 2],
-           [[[[ uvar(1), 6 ], [[ uvar(0), 7 ]]], 2]]]
-
-    ast_to_s(ary_to_sexp(exp)).must_equal expected_ast_string
-
-    res = take_all(a_and_b.call(empty_state))
-    lists_equal?(res, ary_to_sexp(exp)).must_equal true
+    ast_to_s(res).must_equal expected_ast_string
   end
 
   it "ground appendo" do
@@ -104,16 +71,8 @@ describe MicroKanren::Core do
     # (((#(2) b) (#(1)) (#(0) . a)) . 3)
 
     expected_ast_string =
-      "((([2] b) ([1]) ([0] . a)) . 3)"
+      '((([2] b) ([1]) ([0] . a)) . 3)'
 
-    exp = cons(cons(cons(uvar(2), cons(:b, nil)),
-          cons(cons(uvar(1), nil), cons(cons(uvar(0), :a), nil))), 3)
-
-    # Make sure our cons expression above is the same as the expected string.
-    ast_to_s(exp).must_equal expected_ast_string
-
-    # Finally compare the string representation of the result with the string
-    # representation of our cons cell.
     ast_to_s(res).must_equal expected_ast_string
   end
 
@@ -138,25 +97,11 @@ describe MicroKanren::Core do
     # Expected result in scheme:
     # ((() _.0 _.0) ((_.0) _.1 (_.0 . _.1)))
 
-    expected = cons(
-      cons(nil, cons(:'_.0', cons(:'_.0', nil))),
-      cons(cons(cons(:'_.0', nil), cons(:'_.1', cons(cons(:'_.0', :'_.1'), nil))), nil)
-    )
-
     ast_to_s(res).must_equal '((nil _.0 _.0) ((_.0) _.1 (_.0 . _.1)))'
   end
 
   it "reify-1st across appendo2" do
     res = map(method(:reify_1st).to_proc, take(2, call_appendo2.call(empty_state)))
-
-    # Expected result in scheme:
-    # ((() _.0 _.0) ((_.0) _.1 (_.0 . _.1)))
-
-    expected = cons(
-      cons(nil, cons(:'_.0', cons(:'_.0', nil))),
-      cons(cons(cons(:'_.0', nil), cons(:'_.1', cons(cons(:'_.0', :'_.1'), nil))), nil)
-    )
-
     ast_to_s(res).must_equal '((nil _.0 _.0) ((_.0) _.1 (_.0 . _.1)))'
   end
 
