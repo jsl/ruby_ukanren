930b3549a5b67b43f85476b7d777e18d28876113
Justin Leitgeb <justin@stackbuilders.com>
2014-02-01 10:44:35 -0500

    WIP towards Mini Kanren wrappers.

--- lib/micro_kanren/mini_kanren_wrappers.rb
+++ lib/micro_kanren/mini_kanren_wrappers.rb
@@ -1,6 +1,6 @@
 module MicroKanren
   module MiniKanrenWrappers
-    include Lisp
+    include Core
 
     def empty_state
       cons(mzero, 0)
@@ -19,6 +19,49 @@ module MicroKanren
         end
       end
     end
+
+    def zzz(g)
+      -> (a_c) { -> { g.call(a_c) } }
+    end
+
+    def conj_plus(*gs)
+      if gs.length == 1
+        zzz(gs[0])
+      else
+        conj(zzz(gs[0]), conj_plus(gs[1..-1]))
+      end
+    end
+
+    def disj_plus(*gs)
+      if length(gs) == 1
+        zzz(car(gs))
+      else
+        disj(zzz(gs[0]), disj_plus(gs[1..-1]))
+      end
+    end
+
+    def fresh(l, *gs)
+      if l.nil?
+        conj_plus(*gs)
+      else
+        x0 = car(l)
+        call_fresh(-> (x0) { fresh(cdr(l), *gs)})
+      end
+    end
+
+    def conde(*gs)
+      disj_plus(conj_plus(car(gs, car(cdr(gs)))), l)
+    end
+
+    def run_n(n, xs, gs)
+      map(method(:reify_1st).to_proc, take(n, call_goal(fresh(x, gs))))
+    end
+
+    def run_star(xs, *gs)
+      map(method(:reify_1st).to_proc, take_all(call_goal(fresh(xs, *gs))))
+    end
+
+    def call_goal(g) ; g.call(empty_state) ; end
 
     def take_all(stream)
       if cur = pull(stream)
--- spec/micro_kanren/mini_kanren_wrappers_spec.rb
+++ spec/micro_kanren/mini_kanren_wrappers_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe MicroKanren::MiniKanrenWrappers do
+
   include MicroKanren::MiniKanrenWrappers
-  include MicroKanren::Lisp
+
+  # Mostly doing this because some of the methods in MicroKanren::MiniKanrenWrappers
+  # interfere with Minitest methods (eg, #run).
+  # mk = Class.new.extend(MicroKanren::MiniKanrenWrappers)
 
   describe "#pull" do
     it "advances the stream until it matures" do
@@ -12,6 +16,31 @@ describe MicroKanren::MiniKanrenWrappers do
 
     it "returns nil in the case of the empty stream" do
       pull(nil).must_be_nil
+    end
+  end
+
+  def appendo(l, s, out)
+    term1 = eq()
+    conde()
+    if l.nil?
+      eq(nil, l).call(eq(s, out))
+    else
+      to_fresh_list = list(:a, :d, :res)
+      to_fresh_args = [eq(cons(:a, :d), l),
+                       eq(cons(:a, :res), out),
+                       appendo(:d, s, :res)]
+
+      fresh(to_fresh_list, *to_fresh_args)
+    end
+  end
+
+  describe "#run_star" do
+    it "returns the correct result" do
+      res = run_star(list(:q),
+                     fresh(list(:x, :y),
+                     eq(list(:x, :y), :q)),
+                     appendo(:x, :y, list(1, 2, 3, 4, 5)))
+      puts "got #{res.inspect}"
     end
   end
 end
